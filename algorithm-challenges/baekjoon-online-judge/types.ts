a7f99d21b924ea835d8f6a0d64f082d8f27ae913
retn0 <nbsp1221@gmail.com>
2020-10-13 01:19:01 +0900

    🔨 Create types.ts

---
+++ algorithm-challenges/baekjoon-online-judge/types.ts
@@ -0,0 +1,57 @@
+export type Level =
+  | 'Bronze V'
+  | 'Bronze IV'
+  | 'Bronze III'
+  | 'Bronze II'
+  | 'Bronze I'
+  | 'Silver V'
+  | 'Silver IV'
+  | 'Silver III'
+  | 'Silver II'
+  | 'Silver I'
+  | 'Gold V'
+  | 'Gold IV'
+  | 'Gold III'
+  | 'Gold II'
+  | 'Gold I'
+  | 'Platinum V'
+  | 'Platinum IV'
+  | 'Platinum III'
+  | 'Platinum II'
+  | 'Platinum I'
+  | 'Diamond V'
+  | 'Diamond IV'
+  | 'Diamond III'
+  | 'Diamond II'
+  | 'Diamond I'
+  | 'Ruby V'
+  | 'Ruby IV'
+  | 'Ruby III'
+  | 'Ruby II'
+  | 'Ruby I';
+
+export type Tag =
+  | 'Backtracking'
+  | 'BFS'
+  | 'Binary Search'
+  | 'Branch and Bound'
+  | 'DFS'
+  | 'Dijkstra\'s Algorithm'
+  | 'Divide and Conquer'
+  | 'Dynamic Programming'
+  | 'Graph'
+  | 'Longest Increasing Subsequence'
+  | 'Mathematics'
+  | 'Recursion'
+  | 'Simulation';
+
+export interface Challenge {
+  id: number;
+  title: string;
+  extension: string;
+  level: Level;
+  tags: Tag[];
+}
+
+export type ChallengeData = Pick<Challenge, 'title' | 'level' | 'tags'>;
+export type ChallengeFile = Pick<Challenge, 'id' | 'extension'>;
